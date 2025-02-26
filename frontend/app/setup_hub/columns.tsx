0783f8215ed83ab278f7ae740dd45d7e1b1d1fdd
LewisDean22 <lewis@LewisPC.>
2025-02-26 06:50:39 +0000

    First commit on branch, added S1 functions to backend and made manual_roi page

--- frontend/app/setup_hub/columns.tsx
+++ frontend/app/setup_hub/columns.tsx
@@ -1,36 +1,36 @@
-"use client"
+// "use client"
 
-import { ColumnDef } from "@tanstack/react-table"
+// import { ColumnDef } from "@tanstack/react-table"
 
-export type Setup = {
-  id: number
-  name: string
-  date_created: string
-  date_modified: string
-  experiments_used_in: readonly string[]
-  block_x_dimension: number
-  block_x_dimension_unc: number
-  block_y_dimension: number
-  block_y_dimension_unc: number
-  block_z_dimension: number
-  block_z_dimension_unc: number
-  block_refractive_index: number
-  block_refractive_index_unc: number
-  e_log_entry: Blob
-}
+// export type Setup = {
+//   id: number
+//   name: string
+//   date_created: string
+//   date_modified: string
+//   experiments_used_in: readonly string[]
+//   block_x_dimension: number
+//   block_x_dimension_unc: number
+//   block_y_dimension: number
+//   block_y_dimension_unc: number
+//   block_z_dimension: number
+//   block_z_dimension_unc: number
+//   block_refractive_index: number
+//   block_refractive_index_unc: number
+//   e_log_entry: Blob
+// }
 
 
-export const setup_columns: ColumnDef<Setup>[] = [
-  {
-    accessorKey: "name",
-    header: "Name",
-  },
-  {
-    accessorKey: "date_created",
-    header: "Date created",
-  },
-  {
-    accessorKey: "date_modified",
-    header: "Date modified",
-  },
-]
+// export const setup_columns: ColumnDef<Setup>[] = [
+//   {
+//     accessorKey: "name",
+//     header: "Name",
+//   },
+//   {
+//     accessorKey: "date_created",
+//     header: "Date created",
+//   },
+//   {
+//     accessorKey: "date_modified",
+//     header: "Date modified",
+//   },
+// ]
